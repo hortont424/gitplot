2305ba9b623760b71c9e3fa0d0bfd001aee1c569
Tim Horton <hortont424@gmail.com>
2010-05-15 05:43:38 -0400

    good mathematica output

--- gitplot.py
+++ gitplot.py
@@ -4,13 +4,15 @@ from dulwich.repo import Repo
 from datetime import date
 from os.path import basename
 from difflib import Differ
-from pprint import pprint
+from collections import defaultdict
 
 def canonical_author(author):
     if author.find("hortont") > 0:
         return "Tim"
     elif author.find("natesm") > 0:
         return "Nate"
+    elif author.find("racarr") > 0:
+        return "Robb"
     print "Don't know who {0} is!".format(author)
     return author
 
@@ -52,7 +54,48 @@ def list_commit_info(repository):
                basename(repository.path),
                insertions, deletions)
 
-repositories = ["/Users/hortont/src/orbitals",
-                "/Users/hortont/src/Average-Lapse"]
+def single_author_stacked_bar_chart(data, author):
+    data = filter(lambda x: x[0] == author, data)
+    chartData = defaultdict(lambda : defaultdict(int))
 
-print [list(list_commit_info(Repo(r))) for r in repositories]
+    for commit in data:
+        chartData[commit[1]][commit[2]] += commit[3] + commit[4]
+
+    return chartData
+
+def mathematica_stacked_bar_chart(data):
+    days = sorted(data.keys())
+    projects = {}
+
+    # need to inject all dates into list
+
+    for day in days:
+        for project in data[day].keys():
+            projects[project] = 1
+
+    projects = sorted(projects.keys())
+
+    print "BarChart[{"
+    chartStrings = []
+    for day in days:
+        dayStats = []
+        for project in projects:
+            dayStats.append(str(data[day][project]))
+        chartStrings.append("{" + ", ".join(dayStats) + "}")
+    print ",\n".join(chartStrings)
+    print "}, ChartLayout->\"Stacked\"," #ChartLabels->Placed[{",
+    #print ", ".join(["\"" + str(d) + "\"" for d in days]), "}, Below], ",
+    print "ChartLegends->{", ", ".join(["\"" + p + "\"" for p in projects]), "}"
+    print "]"
+
+repositories = ["/Users/hortont/src/Average-Lapse", "/Users/hortont/src/same",
+                "/Users/hortont/src/orbitals", "/Users/hortont/src/phiface",
+                "/Users/hortont/src/particles", "/Users/hortont/src/mbp-video-status",
+                "/Users/hortont/src/seed", "/Users/hortont/src/sheeple", "/Users/hortont/Sites"]
+commitData = []
+
+for repoPath in repositories:
+    commitData.extend(list(list_commit_info(Repo(repoPath))))
+
+chartData = single_author_stacked_bar_chart(commitData, "Tim")
+mathematica_stacked_bar_chart(chartData)
