ca38f141341f86201507f597e9cb56ef46d00674
Tim Horton <hortont424@gmail.com>
2010-05-15 16:21:17 -0400

    Add in extra days (where nothing was done)

--- gitplot.py
+++ gitplot.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 
 from dulwich.repo import Repo
-from datetime import date
+from datetime import date, timedelta
 from os.path import basename
-from difflib import Differ
 from collections import defaultdict
+
+import cProfile
 
 def canonical_author(author):
     if author.find("hortont") > 0:
@@ -13,6 +14,8 @@ def canonical_author(author):
         return "Nate"
     elif author.find("racarr") > 0:
         return "Robb"
+    elif author.find("arsenm") > 0:
+        return "Matt"
     print "Don't know who {0} is!".format(author)
     return author
 
@@ -20,46 +23,26 @@ def get_object_contents(repository, hash):
     return repository.object_store[hash].get_data().splitlines(True)
 
 def list_commit_info(repository):
-    differ = Differ()
     for rev in repository.revision_history(repository.head()):
         parentCommits = rev.get_parents()
-        insertions = deletions = 0
+        insertions = 0
 
         for parent in parentCommits:
             parentTree = repository.object_store[parent].tree
             changes = repository.object_store.tree_changes(parentTree, rev.tree)
-
-            for (_, _, (fromSHA, toSHA)) in changes:
-                if (not fromSHA) and toSHA:
-                    insertions += len(get_object_contents(repository, toSHA))
-                    continue
-
-                if (not toSHA) and fromSHA:
-                    deletions += len(get_object_contents(repository, fromSHA))
-                    continue
-
-                fromFile = get_object_contents(repository, fromSHA)
-                toFile = get_object_contents(repository, toSHA)
-
-                diff = differ.compare(fromFile, toFile)
-
-                for diffLine in list(diff):
-                    if diffLine.startswith("+"):
-                        insertions += 1
-                    elif diffLine.startswith("-"):
-                        deletions += 1
+            insertions += len(list(changes))
 
         yield (canonical_author(rev.author),
                date.fromtimestamp(rev.author_time),
                basename(repository.path),
-               insertions, deletions)
+               insertions)
 
 def single_author_stacked_bar_chart(data, author):
     data = filter(lambda x: x[0] == author, data)
     chartData = defaultdict(lambda : defaultdict(int))
 
     for commit in data:
-        chartData[commit[1]][commit[2]] += commit[3] + commit[4]
+        chartData[commit[1]][commit[2]] += commit[3]
 
     return chartData
 
@@ -67,11 +50,18 @@ def mathematica_stacked_bar_chart(data):
     days = sorted(data.keys())
     projects = {}
 
-    # need to inject all dates into list
+    addingDay = days[0]
+    while addingDay < days[-1]:
+        addingDay += timedelta(1)
+
+        if not addingDay in data:
+            data[addingDay]["null"] = 0
 
     for day in days:
         for project in data[day].keys():
             projects[project] = 1
+
+    days = sorted(data.keys())
 
     projects = sorted(projects.keys())
 
@@ -80,12 +70,12 @@ def mathematica_stacked_bar_chart(data):
     for day in days:
         dayStats = []
         for project in projects:
-            dayStats.append(str(data[day][project]))
+            dayStats.append("Style["+str(data[day][project])+", EdgeForm[]]")
         chartStrings.append("{" + ", ".join(dayStats) + "}")
     print ",\n".join(chartStrings)
-    print "}, ChartLayout->\"Stacked\"," #ChartLabels->Placed[{",
-    #print ", ".join(["\"" + str(d) + "\"" for d in days]), "}, Below], ",
-    print "ChartLegends->{", ", ".join(["\"" + p + "\"" for p in projects]), "}"
+    print "}, ChartLayout->\"Stacked\","
+    print "ChartLegends->{", ", ".join(["\"" + p + "\"" for p in projects]), "},"
+    print "ChartStyle -> \"DarkRainbow\""
     print "]"
 
 repositories = ["/Users/hortont/src/Average-Lapse", "/Users/hortont/src/same",
@@ -94,8 +84,11 @@ repositories = ["/Users/hortont/src/Average-Lapse", "/Users/hortont/src/same",
                 "/Users/hortont/src/seed", "/Users/hortont/src/sheeple", "/Users/hortont/Sites"]
 commitData = []
 
-for repoPath in repositories:
-    commitData.extend(list(list_commit_info(Repo(repoPath))))
+def main():
+    for repoPath in repositories:
+        commitData.extend(list(list_commit_info(Repo(repoPath))))
 
-chartData = single_author_stacked_bar_chart(commitData, "Tim")
-mathematica_stacked_bar_chart(chartData)
+    chartData = single_author_stacked_bar_chart(commitData, "Tim")
+    mathematica_stacked_bar_chart(chartData)
+
+main()
